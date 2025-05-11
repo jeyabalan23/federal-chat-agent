65b8aa326dfe808cd83d317e9a4be9533f438d39
jeyabalan23 <40784498+jeyabalan23@users.noreply.github.com>
2025-05-11 11:21:22 +0200

    Update process_data.py

--- data_pipeline/process_data.py
+++ data_pipeline/process_data.py
@@ -1,1 +1,8 @@
 # Code to process raw federal data
+import pandas as pd
+
+def clean_data(file_path):
+    df = pd.read_csv(file_path)
+    df.dropna(inplace=True)
+    df.columns = [col.strip().lower().replace(' ', '_') for col in df.columns]
+    return df
